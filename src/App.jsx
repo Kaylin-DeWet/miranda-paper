dce1834cf8b69a714fdf302c7e31207ceb51c557
Kaylin-DeWet <kaylind.dewet@gmail.com>
2023-07-19 12:27:55 +0200

    Header update

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,59 @@ const App = () => {
       <div id="main">
         <div id="wrapper" className="bg-[#C5BEB4] h-screen w-screen relative">
           <Navbar />
-          <section>
-            <div>
-              <img
-                src="https://assets.website-files.com/5f9085a4041dd5427c5ac8ae/615d9672cc65f12c9ab25f21_thumbnail-small.jpeg?"
-                alt="table lamp"
-              />
+          <section className="w-full h-[400px] flex items-center justify-between py-10 px-12">
+            <div className="w-[30%]">
+              <div className="h-[60%] overflow-hidden">
+                <img
+                  src="https://assets.website-files.com/5f9085a4041dd5427c5ac8ae/615d9672cc65f12c9ab25f21_thumbnail-small.jpeg?"
+                  alt="table lamp"
+                  className="w-full h-full object-cover hover:scale-110 transition-all cursor-pointer"
+                />
+              </div>
+              <h2 className="font-Canopee mt-3 text-3xl font-semibold">
+                AVRO | KO
+                <span className="text-white  ml-2 border-solid border-4 rounded bg-red-600 border-red-600">
+                  NEW
+                </span>
+              </h2>
+              <p className="font-NewLight text-xl mt-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+                commodi beatae esse excepturi nam impedit vitae magnam deleniti
+                maiores perferendis?
+              </p>
             </div>
-            <div></div>
-            <div>
-              <img
-                src="https://assets.website-files.com/5f9085a4041dd5427c5ac8ae/645b5439577bd35377de8c43_thumbnail-small.webp?"
-                alt="image 2"
-              />
+            <div className="w-[30%] h-[90%] border-x-2 border-gray-500">
+              <h2 className="font-Canopee text-6xl font-semibold text-center">
+                ALL WORK!
+              </h2>
+              <h3 className="font-NewLight text-4xl text-center mt-4 leading-normal">
+                A Featured selection <br /> the latest work - <br /> of the last
+                years.
+              </h3>
+              <h5 className="font-NewLight text-xl text-center mt-6">
+                <span className="font-semibold">TIP!</span> Drag sideways to
+                navigate.
+              </h5>
+            </div>
+            <div className="w-[30%]">
+              <div className="h-[60%] overflow-hidden">
+                <img
+                  src="https://assets.website-files.com/5f9085a4041dd5427c5ac8ae/645b5439577bd35377de8c43_thumbnail-small.webp?"
+                  alt="sneakers"
+                  className="w-full h-full object-cover hover:scale-110 transition-all cursor-pointer"
+                />
+              </div>
+              <h2 className="font-Canopee mt-3 text-3xl font-semibold">
+                THE ROGER HUB
+                <span className="text-white ml-2 border-solid border-4 rounded bg-red-600 border-red-600">
+                  NEW
+                </span>
+              </h2>
+              <p className="font-NewLight text-xl mt-2">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Molestias tempore molestiae, et quisquam quos at ratione soluta
+                ut? Repellendus, saepe!
+              </p>
             </div>
           </section>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
 
 const Navbar = () => {
   return (
     <>
-          <nav className="bg-[#C5BEB4] w-full h-[110px] flex items-center justify-between px-8 border-b-2 border-gray-500">
-            <h1 className="text-2xl">Amsterdam, NL</h1>
-            <img
-              src="https://uploads-ssl.webflow.com/5f2429f172d117fcee10e819/5f7f87c8b81a6e7a214312f0_header.svg"
-              alt="The Paper Portfolio logo"
-              className="h-[40px]"
-            />
-            <HiOutlineMenuAlt4 className="text-2xl"/>
-          </nav>
+      <nav className="bg-[#C5BEB4] w-full h-[110px] flex items-center justify-between px-8 border-b-2 border-gray-500">
+        <h1 className="font-NewLight text-2xl">Amsterdam, NL</h1>
+        <img
+          src="https://uploads-ssl.webflow.com/5f2429f172d117fcee10e819/5f7f87c8b81a6e7a214312f0_header.svg"
+          alt="The Paper Portfolio logo"
+          className="h-[40px]"
+        />
+        <HiOutlineMenuAlt4 className="text-2xl" />
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
